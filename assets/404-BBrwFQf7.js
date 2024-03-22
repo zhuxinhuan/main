cb7dc6cb0c7556b61f3fc721cf2d5a707ad115b9
zhuxinhuan <zhu569523658@126.com>
2024-03-22 15:43:31 +0800

    Updates

--- assets/404-BBrwFQf7.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CTBD-1dE.js";function n(){return r.jsx(r.Fragment,{children:r.jsx("p",{className:" ",children:"404"})})}export{n as default};
---
+++ assets/404-Bx--y4zn.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-B0mDg0yT.js";function n(){return t.jsx(t.Fragment,{children:t.jsx("p",{className:"text-4xl font-bold text-center",children:"404"})})}export{n as default};
